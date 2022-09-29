a7059d068a0b457a4fe269b60a1f17c29e09edb6
Shruthi HN <shruthi.8123hn@gmail.com>
2022-09-29 12:20:04 +0530

    Demo

---
+++ dataType.java
@@ -0,0 +1,23 @@
+class dataType{
+
+ 
+public static void main(String args[]){
+  int a=10;
+  byte b=20;
+ short c=500;
+ long d=600L;
+ float e=123.67787878f;
+ double f=12345.9999999999;
+ char g='2';
+
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+
+}
+}
+
