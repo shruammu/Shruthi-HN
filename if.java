107279ce9235337d7e406c364a6aaac30dd4ba36
Shruthi HN <shruthi.8123hn@gmail.com>
2022-10-06 20:12:34 +0530

    ifs

---
+++ if.java
@@ -0,0 +1,14 @@
+class if{
+ 
+public static void main (String args[]){
+
+System.out.println("Main started");
+int age=20;
+
+if(age==18)
+{ 
+System.out.println("age is 18");
+}
+System.out.println("main ended");
+}
+}
