b7ac08436d47e0195741cbfb567ec9d0185e4427
Shruthi HN <shruthi.8123hn@gmail.com>
2022-10-06 20:17:07 +0530

    nested

---
+++ nesteddemo.java
@@ -0,0 +1,22 @@
+class nesteddemo{
+
+public static void main(String args[]){
+
+String qualification="BE";
+boolean knowJava=true;
+
+if(qualification=="BE"){
+
+if(knowJava){
+
+System.out.println("your eligible to attend interview");
+}
+else{
+System.out.println("your  not eligible to attend interview");
+}
+}
+}
+}
+
+
+ 
