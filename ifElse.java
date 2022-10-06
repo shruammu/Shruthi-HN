7dee58fd8ea22d03d38deba95d3c7a6f31c06c80
Shruthi HN <shruthi.8123hn@gmail.com>
2022-10-06 20:15:39 +0530

    ifel

---
+++ ifElse.java
@@ -0,0 +1,14 @@
+class ifElse{
+ 
+public static void main(String args[]){
+
+int age=15;
+
+if(age>=18){
+
+System.out.println("your eligible to vote");
+}
+else
+System.out.println(" your not eligible to vote");
+}
+}
