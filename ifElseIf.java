14e61187189b45f35cffee9c7f4daf907591628b
Shruthi HN <shruthi.8123hn@gmail.com>
2022-10-06 20:16:20 +0530

    ifelif

---
+++ ifElseIf.java
@@ -0,0 +1,21 @@
+class ifElseIf{
+
+public static void main(String args[]){
+
+double marks=95.5;
+if(marks>=85){
+ System.out.println("boss you passed in Distinction");
+}
+else if(marks>=65 && marks<85)
+{
+System.out.println("boss you passed in first class");
+}
+else if(marks<60 && marks>=35){
+System.out.println("boss you passed in second class");
+}
+else{
+ System.out.println("boss you are failed");
+}
+}
+}
+ 
