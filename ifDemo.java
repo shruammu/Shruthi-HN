e86776dc81dbabb02b4ebba5662fb8de8369cad5
Shruthi HN <shruthi.8123hn@gmail.com>
2022-10-06 20:14:55 +0530

    ifD

---
+++ ifDemo.java
@@ -0,0 +1,14 @@
+class ifDemo{
+ 
+public static void main (String args[]){
+
+System.out.println("Main started");
+int age=18;
+
+if(age==18)
+{ 
+System.out.println("age is 18");
+}
+System.out.println("main ended");
+}
+}
