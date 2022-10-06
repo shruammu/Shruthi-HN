2e4ebd2e9a4ff580f0987a57b8ac4aabb9a54867
Shruthi HN <shruthi.8123hn@gmail.com>
2022-10-06 20:11:32 +0530

    loop

---
+++ forLoopDemo.java
@@ -0,0 +1,14 @@
+class forLoopDemo{
+
+public static void main(String args[])
+{
+ System.out.println("main started");
+  
+ for(int i=1;i<=10;i++)
+{
+  System.out.println("welcome to xworkzzz......");
+}
+System.out.println("main ended");
+}
+
+}
