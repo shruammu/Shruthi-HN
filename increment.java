13a3b6d6688959f8134758261f18af5211ea910c
Shruthi HN <shruthi.8123hn@gmail.com>
2022-10-06 18:30:06 +0530

    increment

---
+++ increment.java
@@ -0,0 +1,25 @@
+class increment{
+ public static void main(String args[]){
+ int x=100;
+int y=++x;
+int z=++y;
+
+System.out.println("x : "+x);
+System.out.println("y : "+y);
+System.out.println("z : "+z);
+ int p =500;
+int q =p++;
+System.out.println("p : "+p);
+System.out.println("q : "+q);
+ int a=10;
+ a++;
+int c=++a;
+System.out.println("a : "+a);
+System.out.println("c : "+(c++));
+System.out.println("c : "+c);
+}
+}
+
+
+
+ 
