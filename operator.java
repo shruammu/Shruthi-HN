464a068b5144aa898aea83227c35fc62f0e80932
Shruthi HN <shruthi.8123hn@gmail.com>
2022-10-06 18:32:38 +0530

    operator

---
+++ operator.java
@@ -0,0 +1,18 @@
+class operator{
+public static void main(String args[]){
+int a=100, b=30;
+int c= a+b;
+int c1=a-b;
+float c2=a/b;
+int c3=a*b;
+float c4=a%b;
+System.out.println(c);
+System.out.println(c1);
+System.out.println(c2);
+System.out.println(c3);
+System.out.println(c4);
+
+}
+}
+
+ 
