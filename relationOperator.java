4386cb77ad0e4795e7d5156a541045e8ae0c0d42
Shruthi HN <shruthi.8123hn@gmail.com>
2022-10-06 18:37:50 +0530

    relation operator

---
+++ relationOperator.java
@@ -0,0 +1,19 @@
+class relationOperator{
+ 
+public static void main(String args[]){
+ int k=60;
+ int l=20;
+ 
+boolean result =k<1;
+
+System.out.println("k : "+k);
+System.out.println("l : "+l);
+System.out.println("k<l : "+result);
+System.out.println("k>1: "+(k>l));
+System.out.println("k==l: "+(k==l));
+System.out.println("k!=l : "+(k!=l));
+System.out.println("k<=l : "+(k<=l));
+System.out.println("k>=l : "+(k>=l));
+
+}
+}
